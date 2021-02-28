fb0fa015a94f828ce69d4395e7c0ba899b10f70a
bambiroe <grimbellz.contact@gmail.com>
2021-02-28 19:02:39 +0100

    Dodatek slike in modifikacija Gravitacija.java #4

--- Gravitacija.java
+++ Gravitacija.java
@@ -5,4 +5,9 @@ public class Gravitacija {
     public static void main(String[] args) {
         System.out.println("OIS je zakon");
     }
+
+    public static void izpis(int v, int g) {
+        System.out.println(v);
+        System.out.println(g);
+    }
 }
