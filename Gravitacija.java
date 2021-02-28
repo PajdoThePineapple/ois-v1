92e9af57717935dd3d9fd5b37e661dbcb215e9db
drKhaki <blaqqula.contact@gmail.com>
2021-02-28 19:02:29 +0100

    Dodatek slike in modifikacija Gravitacija.java #3

--- Gravitacija.java
+++ Gravitacija.java
@@ -5,4 +5,9 @@ public class Gravitacija {
     public static void main(String[] args) {
         System.out.println("OIS je zakon");
     }
+    public static int (int v){
+        int stevec = 6.674*Math.pow(10,-11)*5.972*Math.pow(10,24);
+        int imenovalec = 6.371*Math.pow(10,6) + v;
+        return stevec/(imenovalec*imenovalec)
+    }
 }
