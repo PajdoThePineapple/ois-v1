971c81377de6e55aadc1b171d0609e180d097bfb
PajdoThePineapple <mojca.athena@gmail.com>
2021-02-28 19:22:25 +0100

    JUPIII

--- Gravitacija.java
+++ Gravitacija.java
@@ -3,12 +3,17 @@ import java.util.*;
 public class Gravitacija {
 
     public static void main(String[] args) {
-        System.out.println("OIS je zakon");
+    	Scanner sc = new Scanner(System.in);
+    	int visina = sc.nextInt();
+    	int pospesek = izracun(visina);
+    	izpis(visina, pospesek);
+    	System.out.println("Sej OIS je kul.");
     }
-    public static int (int v){
-        int stevec = 6.674*Math.pow(10,-11)*5.972*Math.pow(10,24);
-        int imenovalec = 6.371*Math.pow(10,6) + v;
-        return stevec/(imenovalec*imenovalec)
+    public static int izracun(int v) {
+        int stevec = 6.674 * Math.pow(10,-11) * 5.972 * Math.pow(10,24);
+        int imenovalec = 6.371 * Math.pow(10,6) + v;
+        return (stevec / (imenovalec * imenovalec));
+    }
 
     public static void izpis(int v, int g) {
         System.out.println(v);
